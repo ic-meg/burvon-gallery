08a77860d014f0f4bbafc4a9b6e62aeaaaf9286c
Meg Angeline Fabian <ic.megangeline.fabian@cvsu.edu.ph>
2025-07-16 17:15:43 +0800

    setup icons and css fonts for easier reuse

---
+++ src/assets/index.js
@@ -0,0 +1,10 @@
+// ICONS
+export { default as IconSearch } from '../assets/icons/search.svg';
+export { default as IconUser } from '../assets/icons/profile.svg';
+export { default as IconHeart } from '../assets/icons/heart.svg';
+export { default as IconBag } from '../assets/icons/cart.svg';
+
+// IMAGES
+export { default as LogoImage } from './logo/black_brv.png';
+export {default as WhiteLogo } from './logo/white_brv.png';
+
