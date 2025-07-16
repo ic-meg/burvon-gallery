af971358f76ef02aaa4c868b5eeb5e3c1651e2a9
Meg Angeline Fabian <ic.megangeline.fabian@cvsu.edu.ph>
2025-07-16 17:14:45 +0800

    setup react, public fonts

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/header";
+
+
+function App() {
+  return (
+    <div className="mettalic-bg">
+    <Header />
+
+    </div>
+  );
+}
+
+export default App;
