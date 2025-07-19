a3e6dad28399dedc5facabc4cf3d5be702db5b58
Meg Angeline Fabian <ic.megangeline.fabian@cvsu.edu.ph>
2025-07-19 22:30:24 +0800

    Created Folder / Files struc

---
+++ src/pages/main/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const Homepage = () => {
+  return (
+    <Layout>
+      {/* 
+        START YOUR HOMEPAGE CONTENT HERE
+
+  
+      */}
+    </Layout>
+  )
+}
+
+export default Homepage
---
+++ src/pages/main/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <p className='avant'> LOGIN PAGE</p>
+    </div>
+  )
+}
+
+export default Login
