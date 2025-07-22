4ebdf3f33601ae96c8c6f93bf870dbbfd2adef63
Meg Angeline Fabian <ic.megangeline.fabian@cvsu.edu.ph>
2025-07-22 15:11:27 +0800

    added comments for collaborators

--- src/pages/main/Homepage.jsx
+++ src/pages/main/Homepage.jsx
@@ -6,9 +6,11 @@ const Homepage = () => {
     <Layout>
       {/* 
         START YOUR HOMEPAGE CONTENT HERE
-
+        
   
       */}
+       {/* pag gumana yung text-purple-500, means nagana yung tailwind css  */}
+      <h1 className='text-purple-500'>Homepage</h1>
     </Layout>
   )
 }
