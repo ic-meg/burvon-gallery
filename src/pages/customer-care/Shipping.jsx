e700122b47ae4b8eb8aa5518c2fbcca325f42984
Meg Angeline Fabian <ic.megangeline.fabian@cvsu.edu.ph>
2025-09-12 01:26:35 +0800

    all pages template

---
+++ src/pages/customer-care/Shipping.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const Shipping = () => {
+  return (
+    <Layout>
+      Shipping 
+    </Layout>
+  )
+}
+
+export default Shipping
---
+++ src/pages/customer-care/SizeGuide.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const SizeGuide = () => {
+  return (
+    <Layout>
+      Size Guide
+    </Layout>
+  )
+}
+
+export default SizeGuide
