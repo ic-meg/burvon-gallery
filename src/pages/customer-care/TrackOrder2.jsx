e863ab2133ac1222a8af4314744ede6f15c310ca
Meg Angeline Fabian <ic.megangeline.fabian@cvsu.edu.ph>
2025-09-13 01:44:32 +0800

    Create TrackOrder2.jsx
    Created the placeholder file

---
+++ src/pages/customer-care/TrackOrder2.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const TrackOrder2 = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>Track Your Order Timeline here</h1>
+      
+      </div>
+    </Layout>
+  )
+}
+
+export default TrackOrder2
