bcd27cc6674d367d91204f25be415e323ebe10cd
Ley <shanleygalo0000@gmail.com>
2025-09-19 19:30:33 +0800

    updated ourstory, jewelrycare, shipping, verification

--- src/pages/main/Login/Verification.jsx
+++ src/pages/main/Login/Verification.jsx
@@ -22,26 +22,41 @@ const Verification = () => {
     return (
       <>
         {/* Mobile Verification */}
-        <div className="flex md:hidden min-h-screen flex flex-col items-center justify-center veri-gray">
-          {/* Logo */}
-          <img src={white_brv} alt="BURVON Logo" className="mb-4 w-40" style={{ marginTop: "-110px" }} />
-          <p className="avant text-white text-lg mb-2" style={{ marginTop: "-70px" }}>Discover Timeless Elegance</p>
-          <p className="avant text-gray-400 text-sm mb-1 text-center">
-            Use the verification link sent to your email
-          </p>
-          <p className="avant text-gray-400 text-sm mb-6 text-center">
-            youremail@gmail.com
-          </p>
-          <button
-            className="py-3 px-6 font-semibold avant border border-black"
+        <div className="flex md:hidden min-h-screen flex flex-col items-center justify-center bg-black relative overflow-hidden">
+          {/* Background image */}
+          <div
+            className="absolute top-0 left-0 w-full h-full bg-cover bg-center z-0"
             style={{
-              borderRadius: "8px",
-              backgroundColor: "#FFF7DC",
-              color: "black",
+              backgroundImage: `url(${loginBG})`,
+              backgroundAttachment: "fixed",
             }}
           >
-            Didn't receive a link? Resend (51)
-          </button>
+            <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 z-10"></div>
+            {/* white overlay */}
+            <div className="absolute top-0 left-0 w-full h-full bg-white opacity-8 z-10"></div>
+
+          </div>
+          {/* Content */}
+          <div className="relative z-20 flex flex-col items-center w-full">
+            <img src={white_brv} alt="BURVON Logo" className="mb-4 w-40" style={{ marginTop: "-110px" }} />
+            <p className="avant text-white text-lg mb-2" style={{ marginTop: "-70px" }}>Discover Timeless Elegance</p>
+            <p className="avant text-gray-400 text-sm mb-1 text-center">
+              Use the verification link sent to your email
+            </p>
+            <p className="avant text-gray-400 text-sm mb-6 text-center">
+              youremail@gmail.com
+            </p>
+            <button
+              className="py-3 px-6 font-semibold avant border border-black"
+              style={{
+                borderRadius: "8px",
+                backgroundColor: "#FFF7DC",
+                color: "black",
+              }}
+            >
+              Didn't receive a link? Resend (51)
+            </button>
+          </div>
         </div>
 
         {/* Desktop Verification */}
@@ -109,42 +124,55 @@ const Verification = () => {
   return (
     <>
       {/* Mobile Terms */}
-      <div className="flex md:hidden min-h-screen flex-col items-center justify-center veri-gray p-6">
-        {/* logo */}
-        <img src={white_brv} alt="BURVON Logo" className="mb-8 w-32" style={{ marginTop: "-110px"}}/>
-        <p className="avant text-white text-lg mb-8 text-center" style={{ color: "#959595", marginTop: "-60px", marginBottom: "16px"}}>
-          Please read and accept the terms to continue
-        </p>
-        
-        <div className="flex items-center mb-4">
-          <input
-            type="checkbox"
-            id="termsCheckbox"
-            checked={acceptedTerms}
-            onChange={(e) => setAcceptedTerms(e.target.checked)}
-            className="mr-3"
-            style={{
-              width: "20px",
-              height: "20px",
-              borderRadius: "50%",
-              appearance: "none",
-              border: "2px solid #959595",
-              backgroundColor: acceptedTerms ? "#FFF7DC" : "transparent",
-            }}
-          />
-          <label htmlFor="termsCheckbox" className="avant text-white" style={{ color: "#959595", fontSize: "12px" }}>
-            I agree to the Terms of Service and Privacy Policy
-          </label>
-        </div>
-        
-        <button
-          className={`w-full py-3 font-semibold avant border border-black ${acceptedTerms ? 'custom-btn-bg' : 'bg-gray-500'}`}
-          style={{ borderRadius: "12px", marginTop: "-3px" }}
-          disabled={!acceptedTerms}
-          onClick={() => navigate('/')} // deretso sa homepage pagka continue
+      <div className="flex md:hidden min-h-screen flex-col items-center justify-center bg-black p-6 relative overflow-hidden">
+        {/* Background image */}
+        <div
+          className="absolute top-0 left-0 w-full h-full bg-cover bg-center z-0"
+          style={{
+            backgroundImage: `url(${loginBG})`,
+            backgroundAttachment: "fixed",
+          }}
         >
-          CONTINUE
-        </button>
+          <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 z-10"></div>
+          {/* white overlay */}
+          <div className="absolute top-0 left-0 w-full h-full bg-white opacity-8 z-10"></div>
+
+        </div>
+        {/* Content */}
+        <div className="relative z-20 flex flex-col items-center w-full">
+          <img src={white_brv} alt="BURVON Logo" className="mb-8 w-32" style={{ marginTop: "-110px"}}/>
+          <p className="avant text-white text-lg mb-8 text-center" style={{ color: "#959595", marginTop: "-60px", marginBottom: "16px", whiteSpace: "nowrap" }}>
+            PLEASE READ AND ACCEPT THE TERMS TO CONTINUE
+          </p>
+          <div className="flex items-center mb-4">
+            <input
+              type="checkbox"
+              id="termsCheckbox"
+              checked={acceptedTerms}
+              onChange={(e) => setAcceptedTerms(e.target.checked)}
+              className="mr-3"
+              style={{
+                width: "20px",
+                height: "20px",
+                borderRadius: "50%",
+                appearance: "none",
+                border: "2px solid #959595",
+                backgroundColor: acceptedTerms ? "#FFF7DC" : "transparent",
+              }}
+            />
+            <label htmlFor="termsCheckbox" className="avant text-white" style={{ color: "#959595", fontSize: "15px" }}>
+              I agree to the Terms of Service and Privacy Policy
+            </label>
+          </div>
+          <button
+            className={`w-full py-3 font-semibold avant border border-black ${acceptedTerms ? 'custom-btn-bg' : 'bg-gray-500'}`}
+            style={{ borderRadius: "12px", marginTop: "-3px" }}
+            disabled={!acceptedTerms}
+            onClick={() => navigate('/')}
+          >
+            CONTINUE
+          </button>
+        </div>
       </div>
 
       {/* Desktop Terms */}
@@ -176,8 +204,9 @@ const Verification = () => {
           <div className="w-full max-w-md">
             {/* logo */}
             <img src={white_brv} alt="BURVON Logo" className="mx-auto mb-20 w-40" style={{ marginTop: "-20px" }} />
-            <p className="avant text-white text-2xl mb-10 text-center" style={{ color: "#959595", marginTop: "-85px"}}>
-              Please read and accept the terms to continue
+            <p className="avant text-white text-xl mb-10 text-center" style={{ color: "#959595", marginTop: "-120px", whiteSpace: "nowrap" }}>
+              PLEASE READ AND ACCEPT THE TERMS TO CONTINUE
+
             </p>
             
             <div className="flex items-start mb-4">
