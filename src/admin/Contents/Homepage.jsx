9ca54e1f5b20f91de132e921c280aeee9f7bab19
Meg Angeline Fabian <ic.megangeline.fabian@cvsu.edu.ph>
2025-10-04 16:35:44 +0800

    Homepage content management file

---
+++ src/admin/Contents/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+//dito ka magcode ng laman ng homepage content management
+const Homepage = () => {
+  return (
+    <div>
+      Homepage content management 
+    </div>
+  )
+}
+
+export default Homepage
