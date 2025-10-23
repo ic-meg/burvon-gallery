03cc048a237ae6e2a4df1343fa1a64b7f9d83505
Meg Angeline Fabian <ic.megangeline.fabian@cvsu.edu.ph>
2025-10-23 19:36:29 +0800

    Update order.module.ts
    added database service in order module first

--- backend/src/order/order.module.ts
+++ backend/src/order/order.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
+import { DatabaseService } from '../database/database.service';
 
 @Module({
   controllers: [OrderController],
-  providers: [OrderService]
+  providers: [OrderService, DatabaseService]
 })
 export class OrderModule {}
