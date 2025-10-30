1e87db68ff53dd78283f9b08f3b871cfc7f93a17
Meg Angeline Fabian <ic.megangeline.fabian@cvsu.edu.ph>
2025-10-31 04:14:07 +0800

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,17 +4,26 @@ config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express.default();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   app.enableCors({
-    origin: true, 
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With'],
     credentials: true,
   });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.init(); 
 }
+
 bootstrap();
+
+export default server; 
