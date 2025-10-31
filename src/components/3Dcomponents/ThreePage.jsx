99bb2644b497223e61ce91e48739e80b861e5c84
Meg Angeline Fabian <ic.megangeline.fabian@cvsu.edu.ph>
2025-10-31 15:46:17 +0800

    Update ThreePage.jsx
    Fix 3D model texture rendering consistency between mobile and desktop

--- src/components/3Dcomponents/ThreePage.jsx
+++ src/components/3Dcomponents/ThreePage.jsx
@@ -2,13 +2,19 @@ import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import { Experience } from "./Experience";
 import GuidedOverlay from "./GuidedOverlay";
+import { ACESFilmicToneMapping } from "three";
 
 export default function ThreePage({ modelPath }) {
   return (
     <div style={{ width: "100%", height: "100%", position: 'relative' }}>
       <Canvas
         style={{ width: "100%", height: "100%" }}
-        gl={{ antialias: true }}
+        gl={{ 
+          antialias: true,
+          toneMapping: ACESFilmicToneMapping,
+          toneMappingExposure: 1,
+          pixelRatio: typeof window !== 'undefined' ? window.devicePixelRatio : 1,
+        }}
         shadows
         camera={{ position: [3, 3, 3], fov: 30 }}
         frameloop="always"
